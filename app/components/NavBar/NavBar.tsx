930604ccef396484849a79ee2f6bf43598e5df7d
Mateo <mateof1223@gmail.com>
2026-07-20 23:48:08 -0400

    Fix Router Usage

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React from "react";
 import MobileMenu from "@/app/components/NavBar/MobileMenu";
 import { useGlobalStore } from "@/app/context/global";
